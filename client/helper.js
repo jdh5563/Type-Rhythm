03bc6b9b47d8bc5d336c70c311e1dadfd767552b
John Heiden <jdh5563@rit.edu>
2022-04-04 11:08:37 -0400

    All views and jsx no longer reference domos

--- client/helper.js
+++ client/helper.js
@@ -4,7 +4,7 @@
 */
 const handleError = (message) => {
     document.getElementById('errorMessage').textContent = message;
-    document.getElementById('domoMessage').classList.remove('hidden');
+    document.getElementById('error').classList.remove('hidden');
 };
 
 /* Sends post requests to the server using fetch. Will look for various
@@ -20,7 +20,7 @@ const sendPost = async (url, data, handler) => {
     });
 
     const result = await response.json();
-    document.getElementById('domoMessage').classList.add('hidden');
+    document.getElementById('error').classList.add('hidden');
 
     if(result.error) {
         handleError(result.error);
@@ -35,6 +35,9 @@ const sendPost = async (url, data, handler) => {
     }
 };
 
+// Sends a post to the given url with the given data.
+// If a handler is given, it will be called after the
+// request is processed
 const sendCarPost = async (url, data, handler) => {
     const response = await fetch(url, {
         method: 'POST',
@@ -55,9 +58,8 @@ const sendCarPost = async (url, data, handler) => {
     }
 };
 
-const hideError = () => {
-    document.getElementById('domoMessage').classList.add('hidden');
-};
+// Hides the error message that is always present on screen
+const hideError = () => document.getElementById('error').classList.add('hidden');
 
 module.exports = {
     handleError,

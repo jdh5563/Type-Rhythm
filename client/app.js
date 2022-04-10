0112cbc4622dc371d3d7ad1b2d77b5b9496edbd5
John Heiden <jdh5563@rit.edu>
2022-04-09 21:02:24 -0400

    Players can now create and join lobbies

--- client/app.js
+++ client/app.js
@@ -44,6 +44,8 @@ const joinLobby = async (raceCode, _csrf) => {
   const usernameResponse = await fetch('/getUsername');
   const usernameData = await usernameResponse.json().then(username => username);
 
+  const lobbyInfo = { username: usernameData, raceCode, _csrf };
+
   const lobbyResponse = await fetch('/joinLobby', {
     method: 'POST',
     headers: {
